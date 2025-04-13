d9d0f0d1dc3b7dde874a1d2dbae3aacaba1b3462
Kashsan <kashsan_jp@yahoo.co.jp>
2025-04-14 01:15:45 +0900

    2025.4.14.13:01

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,15 @@ const Home = () => {
   return (
   
         <div className="container">
-            <h1>Welcome to my website!</h1>
-
-            <p>This page should be protected by a PrivateRoutes component for authenticated users</p>
+            <h1>Kashsanのプロフィール</h1>
+            <p>2017年にSwiftで開発を始める。
+            2020年からReactNative,Flutterの
+            モバイルアプリ開発を始めると同時に
+            React、Vue、Laravel、Nodejsにも
+            興味を持ち開発を始める。<br></br>
+            現在は介護福祉関係の会社を退職し、
+            個人開発に集中してサービス作りを
+            続けています。</p>
         </div>
 
   )
