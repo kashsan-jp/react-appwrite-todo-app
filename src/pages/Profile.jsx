3ce6750168e8ef76012de2a557c795c2d29027c0
Kashsan <kashsan_jp@yahoo.co.jp>
2025-04-14 01:45:07 +0900

    2025.4.14.13:03

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,7 +3,15 @@
 const Profile = () => {
   return (
     <div className="container">
-        <h1>Welcome to my profile!</h1>
+      <h1>Kashsanのプロフィール</h1>
+      <p>2017年にSwiftで開発を始める。
+      2020年からReactNative,Flutterの
+      モバイルアプリ開発を始めると同時に
+      React、Vue、Laravel、Nodejsにも
+      興味を持ち開発を始める。<br></br>
+      現在は介護福祉関係の会社を退職し、
+      個人開発に集中してサービス作りを
+      続けています。</p>
     </div>
   )
 }
