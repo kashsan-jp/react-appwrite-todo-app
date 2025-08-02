ed36c3d9cd56e2ecdbd7be29256da738fc4f1009
Kashsan <kashsan_jp@yahoo.co.jp>
2025-08-02 17:49:47 +0900

    2025.8.2.18:01

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
 
 
 const Home = () => {
-  
+
   console.log("hello kash");
 
   return (
   
-        <div className="container">
-            <h1>Kashsanのアプリ</h1>
-            
-        </div>
+    <div className="container">
+      <h1>Kashsanの技術ノート</h1>
+      <p>フロントエンド: React-Vite</p>
+      <p>バックエンド: AppWrite</p>
+      <p></p>
+    </div>
         
   )
 }
